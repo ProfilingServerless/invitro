0cacd2954b5a01da2e2dbb48d020ccb837d833a7
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-11-07 18:31:34 +0330

    Add kubelet recorder

---
+++ scripts/recorder/kubelet.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+sudo journalctl -f -u kubelet | grep --line-buffered -E 'Receiving a new pod|Creating PodSandbox for pod|Created PodSandbox for pod|Observed pod startup duration' > kubelet.txt
+
