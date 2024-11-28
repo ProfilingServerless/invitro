0d060ba97b2d474818806dbf5bfad8c5bdb6a8d2
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-11-28 18:34:25 +0330

    Add notes

--- run_motivation_1_B.sh
+++ run_motivation_1_B.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# Note.
+# Before running make sure to set the config granularity to second and duration to 90.
+
 EXPS=("5" "10" "15" "20" "25" "30" "35" "40" "45" "50" "55" "60" "65" "70" "75" "80" "85" "90" "95" "100" "105" "110" "115")
 
 server_exec() {
