8a7c0cf1ee28b2b6e0689c87b16f1c590f405943
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-12-24 13:29:34 +0330

    Add volume metrics

--- scripts/recorder/kubelet.sh
+++ scripts/recorder/kubelet.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-sudo journalctl -f -u kubelet | grep --line-buffered -E 'Creating container in pod|Created container in pod|Receiving a new pod|Creating PodSandbox for pod|Created PodSandbox for pod|Observed pod startup duration|SyncPod enter|Pod cgroup created|Processing pod event|Pod is being synced for the first time|SyncLoop ADD|Pod wokrer has started' > kubelet.txt
+sudo journalctl -f -u kubelet | grep --line-buffered -E 'Creating container in pod|Created container in pod|Receiving a new pod|Creating PodSandbox for pod|Created PodSandbox for pod|Observed pod startup duration|SyncPod enter|Pod cgroup created|Processing pod event|Pod is being synced for the first time|SyncLoop ADD|Pod wokrer has started|Pod volumes attached and mounted' > kubelet.txt
 
